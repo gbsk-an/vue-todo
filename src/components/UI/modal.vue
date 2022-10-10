3b4d516a3dbe073bb1662a8ea08742acacf07e4a
Anna Gubskaya <gubskaya.ann@gmail.com>
2022-10-10 19:56:11 +0500

    Add modal for todo-form

---
+++ src/components/UI/modal.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="popup modal" v-if="show" @click.stop="hideModal">
+    <div class="modal-dialog">
+        <div class="modal-content" @click.stop>
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                <button 
+                    type="button" 
+                    class="btn-close" 
+                    aria-label="Close"
+                    @click.stop="hideModal"
+                >
+                </button>
+              </div>
+            <div class="modal-body">
+                <slot>
+            
+                </slot>
+            </div>                
+        </div>            
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'modal',
+    props: {
+        show: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        hideModal() {
+            this.$emit('update:show', false)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.popup {
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    position: fixed;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+}
+</style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,51 +1,90 @@
 <template>
-<section class="vh-100" style="background-color: #e2d5de;">
+<section class="width: 100%" style="background-color: #e2d5de;">
     <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col col-xl-10">    
+            <div class="col col-xl-10">
                 <div class="card" style="border-radius: 15px;">
-                    <div class="card-body p-5">    
-                        <h3 class="mb-3">Things todo today</h3>    
-                        <ToDoForm
-                        @create="addTask"
-                        />
-                        <ToDoList
+                    <div class="card-body p-5">
+                      <div class="mb-5">
+                        <h3 class="mb-4">Things todo today</h3>
+                        <button-default
+                        @click="showModal"
+                        >
+                          Add task
+                        </button-default>
+                        <modal v-model:show="modalVisibility">
+                            <ToDoForm
+                              @create="addTask"
+                            />
+                        </modal>
+                      </div>                                                
+                      <ToDoList
                         :tasks="tasks"
                         @remove="removeTask"
-                        />    
-                    </div> 
+                        v-if="!isTasksLoading"
+                      />
+                      <div v-else>
+                        <p>Loading...</p>
+                      </div>
+                    </div>                
                 </div>
             </div>
+            <button @click="fetchTasks"></button>
         </div>
     </div>
 </section>
-<DefFooter />
+<FooterDefault />
 </template>
   
   <script>
-  import ToDoForm from "@/components/todo-form.vue";
-  import ToDoList from "@/components/todo-list.vue";
-  import DefFooter from "@/components/def-footer.vue"
+  import ToDoForm from "@/components/TodoForm.vue"
+  import ToDoList from "@/components/TodoList.vue"
+  import FooterDefault from "@/components/FooterDefault.vue"
+
+  import axios from "axios"
+
   export default {
     name: "home",
     components: {
-      ToDoForm, ToDoList, DefFooter
+      ToDoForm,
+      ToDoList,
+      FooterDefault
     },
     data() {
       return {
         tasks: [
           {id: 1, title: 'Visit local farmers market', body: 'Pick out some fresh produce'},
-          {id: 2, title: 'Grab a coffee', body: 'Starbucks'},          
+          {id: 2, title: 'Grab a coffee', body: 'Starbucks'},
         ],
+        isTasksLoading: false,
+        modalVisibility: false,
       }
     },
     methods: {
       addTask(task) {
         this.tasks.push(task);
+        this.modalVisibility = false;
       },
       removeTask(task) {
         this.tasks = this.tasks.filter(t => t.id !== task.id)
+      },
+      async fetchTasks() {
+        try {
+          this.isTasksLoading = true
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+          this.tasks = response.data;
+        } catch (e) {
+          alert('Error')
+        } finally {
+          this.isTasksLoading = false;
+        }
+      },
+      showModal() {
+        this.modalVisibility = true;
       }
+    },
+    mounted() {
+      this.fetchTasks();
     }
   }
   </script>
